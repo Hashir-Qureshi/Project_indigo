cff10d638203ca8a94127a406e54cdd1c28dfd81
Fedora Casimir <casimirfdora@gmail.com>
2017-06-19 16:48:32 -0400

    Fedora split the html and PhP files. Created the Query for the database and passed the result as a JSON object.

--- assignment.php
+++ assignment.php
@@ -1,16 +1,22 @@
-<!DOCTYPE html >
-<html>
-<head>
-    <title> Assignment </title>
-    <script src="Assignment_Script.js"></script>
-</head>
-<body id="edit" onload="generate('question','answers')">
-<h2 id="question"></h2>
-<form id="myForm">
-    <div id="answers"></div>
-<input type="button" onclick="getScore('myForm')"value="Grade Me!">
-</form>
+<?php
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+    $dbname = "";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-</body>
-</html>
+$Q_ID = rand(1,5);
+$query = "SELECT Question FROM ch_1 WHERE ID=$Q_ID";
+$A_query = "SELECT Answer,W_Answer_1,W_Answer_2,W_Answer_3 FROM ch_1 WHERE ID=$Q_ID";
+
+$question = $conn->query($query);
+$question = $question->fetch_array(MYSQLI_NUM);
+
+$results = $conn->query($query);
+$results = $results->fetch_array(MYSQLI_NUM);
+
+echo json_encode(array($question, $results));
+
+?>
