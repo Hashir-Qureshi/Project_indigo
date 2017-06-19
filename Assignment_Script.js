fbb5110f8f437a7da76ebc7010b82c1f1f504b4a
Hashir-Qureshi <qureshihashir@yahoo.com>
2017-06-19 16:45:22 -0400

    Hashir modified the Javascript to use ajax and get the question and answers from php.

--- Assignment_Script.js
+++ Assignment_Script.js
@@ -1,8 +1,6 @@
 
 var numQues = 5;
 var numChoi = 4;
-var questions = ["How old is Joshua","How old is Hashir","How old is Harpreet","How old is Fedora","How old is Mr Cosby"];
-var answers = ["21","22","20","19","79"];
 var Q_Array = ["How old is Joshua","How old is Hashir","How old is Harpreet","How old is Fedora","How old is Mr Cosby"];
 var A_Array = ["21","22","20","19","79"];
 function getScore(form) {
@@ -45,36 +43,29 @@ function getScore(form) {
 }
 
 function generate(Ques,ans) {
-    var Q_Array = ["How old is Joshua","How old is Hashir","How old is Harpreet","How old is Fedora","How old is Mr Cosby"];
-    var A_Array = ["21","22","20","19","79"];
-    var work_Array = A_Array.slice();
+    $.ajax({
+        url: 'assignment.php',
+        success: function(output) {
+            var result = $.parseJSON(output);
 
-    var Q_index = Math.floor(Math.random() * (Q_Array.length));
-    var Quest = document.getElementById(Ques);
-    Quest.innerHTML = Q_Array[Q_index];
+            var question = result[0];
+            var work_Array= result[1];
 
+            var Quest = document.getElementById(Ques);
+            Quest.innerHTML = question;
 
+            var answ = document.getElementById(ans);
 
-    var answ = document.getElementById(ans);
+            for (var i = 0; i < 4; i++) {
+                A_index = Math.floor(Math.random() * (work_Array.length));
+                answ.innerHTML = answ.innerHTML + "<input type = 'radio' name=()+question+( value =" + work_Array[A_index] + ">"
+                    + work_Array[A_index] + "<br>";
+                work_Array.splice(A_index, 1);
+            }
 
+        }
+    });
 
-    var choice_Array = new Array(4);
-    choice_Array[0] = work_Array[Q_index];
-    work_Array.splice(Q_index, 1);
-
-    for (var i = 1; i < choice_Array.length; i++) {
-        var A_index = Math.floor(Math.random() * (work_Array.length));
-        choice_Array[i] = work_Array[A_index];
-        work_Array.splice(A_index, 1);
-    }
-
-
-    for (var i = 0; i < 4; i++) {
-        A_index = Math.floor(Math.random() * (choice_Array.length));
-        answ.innerHTML = answ.innerHTML + "<input type = 'radio' name=()+Q_Array[Q_index]+( value =" + choice_Array[A_index] + ">"
-            + choice_Array[A_index] + "<br>";
-        choice_Array.splice(A_index, 1);
-    }
 
 }
 
--- assignment.php
+++ assignment.php
@@ -3,13 +3,16 @@
 <head>
     <title> Assignment </title>
     <script src="Assignment_Script.js"></script>
+
 </head>
 <body id="edit" onload="generate('question','answers')">
 <h2 id="question"></h2>
 <form id="myForm">
     <div id="answers"></div>
 <input type="button" onclick="getScore('myForm')"value="Grade Me!">
+    <div id="test"></div>
 </form>
+
 
 </body>
 </html>
