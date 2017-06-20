699ccff6c7cc64f31951060e21c5fefc9824ddff
shadow2600 <joshuaperez0420@gmail.com>
2017-06-20 14:24:31 -0400

    Created a new branch to save previous javascript code

--- assignment.php
+++ assignment.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html >
 <html>
 <head>
-    <title> Assignment </title>
+    <script src = "Assignment_Script.js" > </script>
+
+    <title>Quiz Questions And Answers</title>
 </head>
-<body>
+
+<body id="edit" onload="generate('Question','answers')">
+<center><h1>Homework Questions</h1><bodyter>
+<p>
+    <form name="quiz">
+</p>
+    <b>
+        <h2 id="Question"></h2></b>
+<div id="answers"></div>
+
+<p>
+        <input type="button"value="Grade Me"onClick="getScore(this.form);">
+        <input type="reset" value="Clear"></p>
+
+    Number of score out of 1 = <input type= text size=15 name= "mark">
+    Score in percentage = <input type=text size=15 name="percentage"><br>
+
+    </form>
+
+<form method="post" name="Form" onsubmit="" action="">
+</form>
 
 </body>
+
 </html>
