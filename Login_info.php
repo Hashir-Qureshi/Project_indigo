01184b534f8c8e21b74779e98e781440e4bd460a
Hashir-Qureshi <qureshihashir@yahoo.com>
2017-06-20 17:52:44 -0400

    Added Comments to the php code and put the login credential in a seperate field.

---
+++ Login_info.php
@@ -0,0 +1,7 @@
+<?php
+//Holds all the credentials for the database.
+
+$servername = "localhost";
+$username = "root";
+$password = "Qureshi1452";
+$dbname = "project indigo";
--- assignment.php
+++ assignment.php
@@ -5,13 +5,15 @@
     <title> Assignment </title>
 </head>
 <body id="edit" onload="generate('question','answers')">
-<div style="margin:auto; width:20%; border: 3px solid #73AD21; text-align: center;" ><?php question();?>
+<div style="margin:auto; width:20%; border: 3px solid #73AD21; text-align: center;">
+    <?php outputQuestion();?>
 <form id="myForm" action="assignment.php" method="post">
     <div id="answers" style="text-align: left;">
-        <?php answers();?>
+        <?php outputAnswers();?>
     </div></br>
     <input type="button" onclick="getScore('myForm')"value="Grade Me!">
 </form>
 </div>
 </body>
+</html>
 
