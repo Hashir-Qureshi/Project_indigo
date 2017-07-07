666843bce2535bb5279c3db2934ab0c0efcc1171
Hashir-Qureshi <qureshihashir@yahoo.com>
2017-07-06 22:32:55 -0400

    Made jQuery alert the user after checking the answer. The screen displays a message with the check value and changes colors appropriately.

--- assignment.php
+++ assignment.php
@@ -13,7 +13,7 @@ if(empty($_SESSION['usedQuestions'])) {
     <script src="JavaScript/Validation.js"></script>
 </head>
 <body id="edit">
-<div> <?php echo "PROGRESS: ". sizeof($_SESSION['usedQuestions']). "/".$_SESSION['MaxQuestions']; ?></div>
+<div id="flag" style="display:none; width:100%; font-weight: bold;"></div>
 <div style="margin:auto; width:20%; border: 3px solid indigo; text-align: center;">
     <h2 id="question">
         <?php echo $_SESSION['question'][0]; ?>
@@ -24,7 +24,7 @@ if(empty($_SESSION['usedQuestions'])) {
             <?php  $answers = $_SESSION['answers']; shuffle($answers);
             foreach ($answers as $answer):?>
 
-                <label>
+                <label id="<?php echo $answer; ?>" style="display:block" >
                     <input id="answer" type="radio" name="answer" value="<?php echo $answer; ?>"> <?php echo $answer; ?>
                 </label> <br>
 
@@ -32,7 +32,9 @@ if(empty($_SESSION['usedQuestions'])) {
 
         </div>
         <input type="submit" name="check" value="Grade Me!">
+
     </form>
+
 </div>
 </body>
 </html>
