e6a6c754b0eda6dba8cc91f05363375d7adb340d
Hashir-Qureshi <qureshihashir@yahoo.com>
2017-07-30 14:43:38 -0400

    fixed indentation

--- assignment.php
+++ assignment.php
@@ -13,21 +13,21 @@ if($assignment->getUsedQuestions() == null) {
 ?>
 
 <!DOCTYPE html>
-    <html>
-    <head>
-        <title> Assignment </title>
-        <!-- Bootstrap -->
-        <link href="CSS/bootstrap.min.css" rel="stylesheet">
-        <!-- Fonts -->
-        <link href="/fonts/Roboto" rel="stylesheet">
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="JavaScript/jquery-3.2.1.js"></script>
-        <script src="JavaScript/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="CSS/bootstrap.min.css">
-        <script src="JavaScript/Validation.js"></script>
-        <link rel="stylesheet" href="CSS/assignment.css">
-    </head>
-    <body id="edit">
+<html>
+<head>
+    <title> Assignment </title>
+    <!-- Bootstrap -->
+    <link href="CSS/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="/fonts/Roboto" rel="stylesheet">
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="JavaScript/jquery-3.2.1.js"></script>
+    <script src="JavaScript/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <script src="JavaScript/Validation.js"></script>
+    <link rel="stylesheet" href="CSS/assignment.css">
+</head>
+<body id="edit">
 
     <div class="container ">
         <div class="row">
@@ -36,32 +36,31 @@ if($assignment->getUsedQuestions() == null) {
                 <div id="question" class=" padding text-align: center;" style="font-size: 24px; font-family: 'Zilla Slab', Serif; font-weight: bold;" >
                     <?php echo $_SESSION['question'][0]; ?>
 
-                 </div>
-
-            <form id="myForm">
-                <div id="answers" style="text-align: left; font-size: 20px;">
-                    <?php  $answers = $_SESSION['answers']; shuffle($answers);
-                    foreach ($answers as $answer):?>
-
-                        <label id="<?php echo $answer; ?>" class="button"  style="font-family: 'Zilla Slab' , Serif; margin-bottom: -3%;" >
-                            <input id="answer" type="radio" name="answer" value="<?php echo $answer; ?>">
-                            <span> <?php echo $answer; ?></span>
-                        </label> <br>
-
-                    <?php endforeach; ?>
-
                 </div>
-                <div id="flag" style="display:none; width:100%; font-weight: bold;"></div>
 
-                <input type="submit" name="check" value="Grade Me!" class="btn-assignment btn btn-default btn-primary btn-block"><br>
-                <input type="button" style="display:none" name="change" value="Next Question" class="btn-assignment btn btn-default btn-primary btn-block">
-                <input type="button" style="display:none" name="finish" value="View Grade" class="btn-assignment btn btn-default btn-primary btn-block" >
+                <form id="myForm">
+                    <div id="answers" style="text-align: left; font-size: 20px;">
+                        <?php  $answers = $_SESSION['answers']; shuffle($answers);
+                        foreach ($answers as $answer):?>
 
-            </form>
+                            <label id="<?php echo $answer; ?>" class="button"  style="font-family: 'Zilla Slab' , Serif; margin-bottom: -3%;" >
+                                <input id="answer" type="radio" name="answer" value="<?php echo $answer; ?>"><span> <?php echo $answer; ?></span>
+                            </label> <br>
 
-        </div>
+                        <?php endforeach; ?>
+
+                    </div>
+                    <div id="flag" style="display:none; width:100%; font-weight: bold;"></div>
+
+                    <input type="submit" name="check" value="Grade Me!" class="btn-assignment btn btn-default btn-primary btn-block"><br>
+                    <input type="button" style="display:none" name="change" value="Next Question" class="btn-assignment btn btn-default btn-primary btn-block">
+                    <input type="button" style="display:none" name="finish" value="View Grade" class="btn-assignment btn btn-default btn-primary btn-block" >
+
+                </form>
+
+            </div>
             <div id="hint" class="wrap-stuff col-md-2" style="text-align: center;">This is the Hint</div>
         </div>
     </div>
-    </body>
-    </html>
+</body>
+</html>
