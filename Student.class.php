9cdfe8f55736712edbd0e070e0df8be2d7f31080
Hashir Qureshi <qureshihashir@yahoo.com>
2018-04-05 19:29:12 -0400

    implemented the Student Handler and tested it. Modified the Student class to output the grade value instead of array

--- Student.class.php
+++ Student.class.php
@@ -2,67 +2,67 @@
 
 class Student{
     private $id, $firstName, $lastName, $connection;
-    
+
     public function __construct($id, $firstName, $lastName, $DBconnection){
         $this->id = $id;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->connection = $DBconnection;
     }
-    
+
     public function loadEnrolledCourses(){
-        
+
         $query = "select c.course_id, c.course_name, c.section_num, c.semester, c.year, e.enrollment_date from course as c inner join enroll as e on  (e.course_id=c.course_id and e.student_id=".$this->id.") inner join teacher as t on t.teacher_id=c.teacher;";
-        
+
         $result = $this->connection->query($query);
-        
+
         $enrolledCourses = array();
-        
+
         while($row = mysqli_fetch_assoc($result)){
             array_push($enrolledCourses, $row);
         }
-        
+
         return $enrolledCourses;
-        
+
     }
-    
+
     public function enrollCourse($courseID){
         $query = "insert into enroll (course_id, student_id) values (".$courseID.", ".$this->id.");";
-        
+
         $result = $this->connection->query($query);
-        
+
         if($result){
             return true;
         }else return false;
     }
-    
+
     public function loadAssignments($courseID){
         $query = "select a.assignment_name, a.total_points, u.start_date, u.end_date from assignment as a inner join used_assignments as u on (a.assignment_id = u.assignment_id AND u.course_id =".$courseID.");";
-        
+
         $result = $this->connection->query($query);
-        
+
         $assignments = array();
-        
+
         while($row = mysqli_fetch_assoc($result)){
             array_push($assignments, $row);
         }
-        
+
         return $assignments;
-        
-        
+
+
     }
-    
+
     public function getGrade($assignmentID){
-        
+
         $query = "select grade from assignments_taken where student_id = ".$this->id." AND assignment_id = ".$assignmentID.";";
-        
+
         $result = $this->connection->query($query);
-        
+        $grade = $result->fetch_array(MYSQLI_NUM)[0];
 
-        return $result->fetch_array(MYSQLI_NUM);
+        return $grade;
 
-        
-        
+
+
     }
-    
+
 }
--- Student.handler.php
+++ Student.handler.php
@@ -1,13 +1,55 @@
 <?php
 require 'config/DB.connection.php';
-require 'Student.class.php';
+require 'Student.class.php'; 
+
+
 
 $student = new Student(2, 'Hashir', 'Qureshi', $conn);
 
-//var_dump($student->loadEnrolledCourses());
-//
-//var_dump($student->enrollCourse(2));
 
-//var_dump($student->loadAssignments(1));
+//Loading enrolled courses. Displays course info in a table.
+if(isset($_POST['loadEnrolledCourses'])){
+    $courses = $student->loadEnrolledCourses();
+    echo "<table>";
+    foreach($courses as $courseInfo){
+        echo "<tr>";
+        foreach($courseInfo as $info){
 
-var_dump($student->getGrade(1));
+            echo "<td>".htmlentities($info)."</td>";
+        }
+        echo "</tr>";
+    } 
+}
+
+//Enroll for a course. Gives error message if enrollment fails.
+if(isset($_POST['enroll'])){
+    $courseID = mysqli_real_escape_string($conn, $_POST['courseCode']);
+    if($student->enrollCourse($courseID)){
+        echo "Enrollment Succesful!";
+    }else echo "Enrollment Failed! You may already be enrolled in this course or the course code is invalid. Please confirm the course code and try again.";
+}
+
+
+//Load all the assignments for a course. Displays the assignment information
+if(isset($_POST['loadAssignments'])){
+    $courseID = $_POST['courseCode'];
+
+    $assignments = $student->loadAssignments($courseID);
+    echo "<table>";
+    foreach($assignments as $assignemntInfo){
+        echo "<tr>";
+        foreach($assignemntInfo as $info){
+
+            echo "<td>".htmlentities($info)."</td>";
+        }
+        echo "</tr>";
+    } 
+
+}
+
+
+//Get the grade for an assignment
+if(isset($_POST['getGrade'])){
+    $assignmentID = $_POST['assignmentID'];
+    echo $student->getGrade($assignmentID);
+}
